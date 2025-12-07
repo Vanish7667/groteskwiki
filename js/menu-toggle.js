bb23e4d127b340063ef430584d91cb1d20b3313b
Vanish7667 <Groteskstalker@gmail.com>
2025-12-07 23:26:39 +0300

    адаптация

--- js/menu-toggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector("nav ul");
-    if (toggle) {
-        toggle.addEventListener("click", () => {
-            menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-        });
-    }
-});
