e58774838e5abd63335c44cfa20a0e2c3e84a4e8
Vanish7667 <Groteskstalker@gmail.com>
2025-12-14 19:03:00 +0300

    добавление вкладок аренды и фурнитуры

---
+++ js/furniture.js
@@ -0,0 +1,20 @@
+const grid = document.getElementById("furnitureGrid");
+
+fetch("data/furniture/furniture.json")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(item => {
+            const card = document.createElement("div");
+            card.className = "furniture-card";
+
+            card.innerHTML = `
+                <img src="${item.image}" alt="${item.name}">
+                <h3>${item.name}</h3>
+                <p class="description">${item.description}</p>
+                <div class="price">${item.price}</div>
+            `;
+
+            grid.appendChild(card);
+        });
+    })
+    .catch(err => console.error("Ошибка загрузки фурнитуры:", err));
